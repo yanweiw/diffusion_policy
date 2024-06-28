architectural changes to allow two stacked cam images at frequecy lower than pose_ee, turn off imagenet normalization, can train bug-free

diffusion_policy/dataset/real_kitchen_dataset.py
@@ -17,6 +17,8 @@ class RealKitchenDataset(BaseImageDataset):
     def __init__(self,
             zarr_path, 
             horizon=1,
+            dataset_obs_steps=1,
+            n_img_skips=1,
             pad_before=0,
             pad_after=0,
             seed=42,
@@ -26,7 +28,7 @@ class RealKitchenDataset(BaseImageDataset):
         
         super().__init__()
         self.replay_buffer = ReplayBuffer.copy_from_path(
-            zarr_path, keys=['img', 'state', 'action'])
+            zarr_path, keys=['wrist', 'scene', 'state', 'action'])
         val_mask = get_val_mask(
             n_episodes=self.replay_buffer.n_episodes, 
             val_ratio=val_ratio,
@@ -45,6 +47,8 @@ class RealKitchenDataset(BaseImageDataset):
             episode_mask=train_mask)
         self.train_mask = train_mask
         self.horizon = horizon
+        self.dataset_obs_steps = dataset_obs_steps
+        self.n_img_skips = n_img_skips
         self.pad_before = pad_before
         self.pad_after = pad_after
 
@@ -63,24 +67,29 @@ class RealKitchenDataset(BaseImageDataset):
     def get_normalizer(self, mode='limits', **kwargs):
         data = {
             'action': self.replay_buffer['action'],
-            'eef_pose': self.replay_buffer['state']
+            'pose_ee': self.replay_buffer['state']
         }
         normalizer = LinearNormalizer()
         normalizer.fit(data=data, last_n_dims=1, mode=mode, **kwargs)
-        normalizer['image'] = get_image_range_normalizer()
+        normalizer['wrist'] = get_image_range_normalizer()
+        normalizer['scene'] = get_image_range_normalizer()
         return normalizer
 
     def __len__(self) -> int:
         return len(self.sampler)
 
     def _sample_to_data(self, sample):
-        eff_pose = sample['state'].astype(np.float32)
-        image = np.moveaxis(sample['img'],-1,1)/255
-
+        pose_ee = sample['state'].astype(np.float32)[:self.dataset_obs_steps]
+        wrist = np.moveaxis(sample['wrist'],-1,1)/255 
+        scene = np.moveaxis(sample['scene'],-1,1)/255
+        wrist = wrist[self.n_img_skips-1:self.dataset_obs_steps:self.n_img_skips] # start index 7, step 8 so that latest obs is included
+        scene = scene[self.n_img_skips-1:self.dataset_obs_steps:self.n_img_skips]
+        
         data = {
             'obs': {
-                'image': image, # T, 3, 480, 640
-                'eef_pose': eff_pose, # T, 8
+                'wrist': wrist, # T//8, 3, 480, 640
+                'scene': scene, # T//8, 3, 480, 640
+                'pose_ee': pose_ee, # T, 8
             },
             'action': sample['action'].astype(np.float32) # T, 8
         }

diffusion_policy/model/vision/multi_image_obs_encoder.py
@@ -20,7 +20,7 @@ class MultiImageObsEncoder(ModuleAttrMixin):
             # use single rgb model for all rgb inputs
             share_rgb_model: bool=False,
             # renormalize rgb input with imagenet normalization
-            # assuming input in [0,1]
+            # assuming input in [-1,1] after normalizer normalization
             imagenet_norm: bool=False
         ):
         """
@@ -67,6 +67,17 @@ class MultiImageObsEncoder(ModuleAttrMixin):
                                 num_groups=x.num_features//16, 
                                 num_channels=x.num_features)
                         )
+                    # change conv1 to accept stacked images
+                    n_obs_steps = shape_meta['n_obs_steps']
+                    n_img_skips = shape_meta['n_img_skips']
+                    this_model.conv1 = nn.Conv2d(
+                        in_channels=shape[0]*n_obs_steps//n_img_skips,
+                        out_channels=64,
+                        kernel_size=(7, 7),
+                        stride=(2, 2),
+                        padding=(3, 3),
+                        bias=False
+                    )
                     key_model_map[key] = this_model
                 
                 # configure resize
@@ -80,7 +91,7 @@ class MultiImageObsEncoder(ModuleAttrMixin):
                     this_resizer = torchvision.transforms.Resize(
                         size=(h,w)
                     )
-                    input_shape = (shape[0],h,w)
+                    input_shape = (shape[0]*n_obs_steps//n_img_skips,h,w)
 
                 # configure randomizer
                 this_randomizer = nn.Identity()
@@ -159,7 +170,8 @@ class MultiImageObsEncoder(ModuleAttrMixin):
                     batch_size = img.shape[0]
                 else:
                     assert batch_size == img.shape[0]
-                assert img.shape[1:] == self.key_shape_map[key]
+                assert img.shape[2:] == self.key_shape_map[key]
+                img = img.reshape(batch_size, -1, *img.shape[3:]) # reshape from (B,T,C,H,W) to (B,T*C,H,W)
                 img = self.key_transform_map[key](img)
                 feature = self.key_model_map[key](img)
                 features.append(feature)
@@ -171,7 +183,8 @@ class MultiImageObsEncoder(ModuleAttrMixin):
                 batch_size = data.shape[0]
             else:
                 assert batch_size == data.shape[0]
-            assert data.shape[1:] == self.key_shape_map[key]
+            assert data.shape[2:] == self.key_shape_map[key]
+            data = data.reshape(batch_size, -1) # reshape from (B,T,D) to (B,T*D)
             features.append(data)
         
         # concatenate all features
@@ -182,13 +195,21 @@ class MultiImageObsEncoder(ModuleAttrMixin):
     def output_shape(self):
         example_obs_dict = dict()
         obs_shape_meta = self.shape_meta['obs']
+        n_obs_steps = self.shape_meta['n_obs_steps']
+        n_img_skips = self.shape_meta['n_img_skips']
         batch_size = 1
         for key, attr in obs_shape_meta.items():
             shape = tuple(attr['shape'])
-            this_obs = torch.zeros(
-                (batch_size,) + shape, 
-                dtype=self.dtype,
-                device=self.device)
+            if attr['type'] == 'low_dim':
+                this_obs = torch.zeros(
+                    (batch_size,n_obs_steps,) + shape, 
+                    dtype=self.dtype,
+                    device=self.device)
+            else:
+                this_obs = torch.zeros(
+                    (batch_size,n_obs_steps//n_img_skips,) + shape, 
+                    dtype=self.dtype,
+                    device=self.device)
             example_obs_dict[key] = this_obs
         example_output = self.forward(example_obs_dict)
         output_shape = example_output.shape[1:]

diffusion_policy/policy/diffusion_unet_image_policy.py
@@ -45,7 +45,7 @@ class DiffusionUnetImagePolicy(BaseImagePolicy):
         global_cond_dim = None
         if obs_as_global_cond:
             input_dim = action_dim
-            global_cond_dim = obs_feature_dim * n_obs_steps
+            global_cond_dim = obs_feature_dim #* n_obs_steps
 
         model = ConditionalUnet1D(
             input_dim=input_dim,
@@ -147,16 +147,19 @@ class DiffusionUnetImagePolicy(BaseImagePolicy):
         local_cond = None
         global_cond = None
         if self.obs_as_global_cond:
-            # condition through global feature
-            this_nobs = dict_apply(nobs, lambda x: x[:,:To,...].reshape(-1,*x.shape[2:]))
-            nobs_features = self.obs_encoder(this_nobs)
-            # reshape back to B, Do
-            global_cond = nobs_features.reshape(B, -1)
+            # # condition through global feature
+            # this_nobs = dict_apply(nobs, lambda x: x[:,:To,...].reshape(-1,*x.shape[2:]))
+            # nobs_features = self.obs_encoder(this_nobs)
+            # # reshape back to B, Do
+            # global_cond = nobs_features.reshape(B, -1)
+            this_nobs = dict_apply(nobs, lambda x: x[:,:self.n_obs_steps,...]) # do not reshape B, T, ... to B*T
+            global_cond = self.obs_encoder(this_nobs)
+
             # empty data for action
             cond_data = torch.zeros(size=(B, T, Da), device=device, dtype=dtype)
             cond_mask = torch.zeros_like(cond_data, dtype=torch.bool)
             if self.past_action_visible:
-                cond_data[:,:To-1,:] = this_nobs['eef_pose'].unsqueeze(0).expand(B, To, Da)[:,1:,:]
+                cond_data[:,:To-1,:] = this_nobs['pose_ee'][:,1:,:]
                 cond_mask[:,:To-1,:] = True
         else:
             # condition through impainting
@@ -210,13 +213,15 @@ class DiffusionUnetImagePolicy(BaseImagePolicy):
         trajectory = nactions
         cond_data = trajectory
         if self.obs_as_global_cond:
-            # reshape B, T, ... to B*T
-            this_nobs = dict_apply(nobs, 
-                lambda x: x[:,:self.n_obs_steps,...].reshape(-1,*x.shape[2:]))
-            nobs_features = self.obs_encoder(this_nobs)
-            # reshape back to B, Do
-            global_cond = nobs_features.reshape(batch_size, -1)
-        else:
+            # # reshape B, T, ... to B*T
+            # this_nobs = dict_apply(nobs, 
+            #     lambda x: x[:,:self.n_obs_steps,...].reshape(-1,*x.shape[2:]))
+            # nobs_features = self.obs_encoder(this_nobs)
+            # # reshape back to B, Do
+            # global_cond = nobs_features.reshape(batch_size, -1)
+            this_nobs = dict_apply(nobs, lambda x: x[:,:self.n_obs_steps,...]) # do not reshape B, T, ... to B*T
+            global_cond = self.obs_encoder(this_nobs)
+        else: # this may not work with different T dimension for img and lowdim obs
             # reshape B, T, ... to B*T
             this_nobs = dict_apply(nobs, lambda x: x.reshape(-1, *x.shape[2:]))
             nobs_features = self.obs_encoder(this_nobs)

diffusion_policy/scripts/pickle2zarr.py
@@ -17,32 +17,41 @@ def main(datasource):
     episode_ends = []
     state_list = []
     action_list = []
-    img_list = []
+    wristrgb_list = []
+    scenergb_list = []
 
     for file in tqdm(data_files):
         filepath = os.path.join(data_folder, file)
         print('loading file: ', filepath)
         with open(filepath, 'rb') as handle:
                 (xyz, quat, ee, jointpos, scenergb, wristrgb, scenedep, wristdep) = pickle.load(handle)
-        # plan = np.concatenate([plan[0::10], plan[[-1]]], axis=0) # select every 10th step, making sure the last step is included
-        state = np.concatenate([xyz, quat, ee], axis=1) # end effector position and gripper state
+        
+        # end effector position and gripper state
+        state = np.concatenate([xyz, quat, ee], axis=1) 
         print('state shape: ', state.shape)
-        next_xyz = np.concatenate([xyz[1:], xyz[[-1]]], axis=0)
-        next_quat = np.concatenate([quat[1:], quat[[-1]]], axis=0)
-        next_ee = np.concatenate([ee[1:], ee[[-1]]], axis=0)
-        action = np.concatenate([next_xyz, next_quat, next_ee], axis=1) # end effector position and gripper state
+        action = np.concatenate([state[1:], state[[-1]]], axis=0)
         print('action shape: ', action.shape)
+        
         assert action.shape[0] == len(wristrgb), f"action shape: {action.shape}, wristrgb shape: {wristrgb.shape}"
-        img = []
+        assert action.shape[0] == len(scenergb), f"action shape: {action.shape}, scenergb shape: {scenergb.shape}"
+        wrist = []
         for rgb in wristrgb:
             rgb = cv2.resize(rgb, (320, 240))
-            img.append(cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)) 
-        img = np.stack(img)
-        print('obs shape: ', img.shape)
+            wrist.append(cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)) 
+        wrist = np.stack(wrist)
+        print('obs shape: ', wrist.shape)
+
+        scene = []
+        for rgb in scenergb:
+            rgb = cv2.resize(rgb, (320, 240))
+            scene.append(cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB))
+        scene = np.stack(scene)
+        print('scene shape: ', scene.shape)
         
         state_list.append(state)
         action_list.append(action)
-        img_list.append(img)
+        wristrgb_list.append(wrist)
+        scenergb_list.append(scene)
         episode_end += len(action)
         episode_ends.append(episode_end) 
 
@@ -51,7 +60,8 @@ def main(datasource):
     data = data_root.create_group('data')
     data.create_dataset('state', data=np.concatenate(state_list, axis=0), dtype='float32')
     data.create_dataset('action', data=np.concatenate(action_list, axis=0), dtype='float32')
-    data.create_dataset('img', data=np.concatenate(img_list, axis=0), dtype='uint8')
+    data.create_dataset('wrist', data=np.concatenate(wristrgb_list, axis=0), dtype='uint8')
+    data.create_dataset('scene', data=np.concatenate(scenergb_list, axis=0), dtype='uint8')
     meta = data_root.create_group('meta')
     meta.create_dataset('episode_ends', data=np.array(episode_ends))
     print('data saved to: ', os.path.abspath(save_path))